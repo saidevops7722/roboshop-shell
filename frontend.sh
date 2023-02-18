41591b0f77160f74a0eba192355113043b1987d5
saidevops7722 <saiaws7722@gmail.com>
2023-02-18 17:34:08 +0530

    frontend content

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,5 @@
+code_dir=$(pwd)
+
 echo -e "\e[35mInstalling nginx\e[0m"
 yum install nginx -y
 
@@ -12,7 +14,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 echo -e "\e[35mCopying the nginx conf for roboshop\e[0m"
-cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 echo -e "\e[35mEnabling the nginx\e[0m"
 systemctl enable nginx
