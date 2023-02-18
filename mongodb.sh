db1f2d17eefa6fb8ed67cf7c56959806ab05fc72
saidevops7722 <saiaws7722@gmail.com>
2023-02-18 12:03:51 +0530

    roboshop components

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,6 @@
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp configs/mongodb.repo /etc/yum.repos.d/mongod.repo
 yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
-
+systemctl restart mongod
 #update the /etc/mongod.conf from 127.0.0.1 to 0.0.0.0
