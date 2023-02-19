e7f3b066cc6165409ed4423d777661448f8fbeed
saidevops7722 <saiaws7722@gmail.com>
2023-02-19 16:36:19 +0530

    frontend content

--- frontend.sh
+++ frontend.sh
@@ -6,26 +6,26 @@ print_head() {
   echo -e "/e[35m/e[0m"
   }
 
-print_head"installing nginx"
+print_head "installing nginx"
 yum install nginx -y &>>${log_file}
 
-print_head"removing old content"
+print_head "removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 
-print_head"downloading frontend content"
+print_head "downloading frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
-print_head"extracting the downloaded content"
+print_head "extracting the downloaded content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
 
-print_head"copying the nginx conf for roboshop"
+print_head "copying the nginx conf for roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
-print_head"enabling the nginx"
+print_head "enabling the nginx"
 systemctl enable nginx &>>${log_file}
 
-print_head"restarting the nginx"
+print_head "restarting the nginx"
 systemctl restart nginx &>>${log_file}
 
 ## roboshop configs are not copied yet
