c28aa6546505c12b70c5f9ad052ca3fcf6ed64a8
saidevops7722 <saiaws7722@gmail.com>
2023-02-19 16:33:49 +0530

    frontend content

--- frontend.sh
+++ frontend.sh
@@ -2,26 +2,30 @@ code_dir=$(pwd)
 log_file=/tmp/roboshop.log
 rm -f ${log_file}
 
-echo -e "\e[35mInstalling nginx\e[0m"
+print_head() {
+  echo -e "/e[35m/e[0m"
+  }
+
+print_head"installing nginx"
 yum install nginx -y &>>${log_file}
 
-echo -e "\e[35mRemoving old content\e[0m"
+print_head"removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 
-echo -e "\e[35mDownloading the frontend conten\e[0m"
+print_head"downloading frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
-echo -e "\e[35mExtracting downloaded frontend content\e[0m"
+print_head"extracting the downloaded content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
 
-echo -e "\e[35mCopying the nginx conf for roboshop\e[0m"
+print_head"copying the nginx conf for roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
-echo -e "\e[35mEnabling the nginx\e[0m"
+print_head"enabling the nginx"
 systemctl enable nginx &>>${log_file}
 
-echo -e "\e[35mRestarting the nginx\e[0m"
+print_head"restarting the nginx"
 systemctl restart nginx &>>${log_file}
 
 ## roboshop configs are not copied yet
