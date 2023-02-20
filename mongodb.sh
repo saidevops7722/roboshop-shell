dfcc65bddf83739755a366472d638e70071a5e30
saidevops7722 <saiaws7722@gmail.com>
2023-02-20 12:15:30 +0530

    frontend contents

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 source common.sh
 
 print_head "copying the mongodb repo files"
-cp ${code_dir} configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 print_head "installing mongodb"
 yum install mongodb-org -y &>>${log_file}
