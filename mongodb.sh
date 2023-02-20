36995eb84456d6278498d45e832f1224b5de285f
saidevops7722 <saiaws7722@gmail.com>
2023-02-20 12:57:35 +0530

    frontend contents

--- mongodb.sh
+++ mongodb.sh
@@ -7,7 +7,7 @@ print_head "installing mongodb"
 yum install mongodb-org -y &>>${log_file}
 
 print_head "update mongodb listen address"
-sed -i -e "s/127.0.0.1/0.0.0.0" /etc/mongod.conf
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf
 
 print_head "enabling mongodb"
 systemctl enable mongod &>>${log_file}
@@ -15,6 +15,6 @@ systemctl enable mongod &>>${log_file}
 print_head "starting mongodb service"
 systemctl restart mongod &>>${log_file}
 
-#update the /etc/mongod.conf from 127.0.0.1 with 0.0.0.0
+
 
 
