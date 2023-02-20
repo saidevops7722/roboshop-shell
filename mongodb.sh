c3d5b7b64da060631bbd7961babc1116623be67d
saidevops7722 <saiaws7722@gmail.com>
2023-02-20 11:21:17 +0530

    frontend contents

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,17 @@
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
+source common.sh
+
+print-head "copying the mongodb repo files"
+cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+
+print-head "installing mongodb"
+yum install mongodb-org -y &>>${log_file}
+
+print-head "enabling mongodb"
+systemctl enable mongod &>>${log_file}
+
+print-head "starting mongodb service"
+systemctl start mongod &>>${log_file}
+
 #update the /etc/mongod.conf from 127.0.0.1 with 0.0.0.0
 
 
