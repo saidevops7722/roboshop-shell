a68030ba28e60549174373edde2063b71bb95732
saidevops7722 <saiaws7722@gmail.com>
2023-02-23 12:03:32 +0530

    frontend contents

--- shipping.sh
+++ shipping.sh
@@ -1,7 +1,7 @@
 source common.sh
  mysql_root_password=$1
 
- if [ -z "${my_sql_root_password}" ];then
+ if [ -z "${mysql_root_password}" ];then
    echo -e "\e[31m missing mysql root password argument\e[0m"
    exit 1
    fi
