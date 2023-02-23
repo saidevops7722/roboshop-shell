8820d86cb87e4bf28fbbce40378ed7d25cd44abf
saidevops7722 <saiaws7722@gmail.com>
2023-02-23 12:11:03 +0530

    frontend contents

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -2,7 +2,7 @@ source common.sh
  roboshop_app_password=$1
 
  if [ -z "${roboshop_app_password}" ];then
-   echo -e "\e[31m missing mysql root password argument\e[0m"
+   echo -e "\e[31m missing rabbitmq app user password argument\e[0m"
    exit 1
    fi
 
