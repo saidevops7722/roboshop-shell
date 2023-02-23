bdcfe31a4625d1eeb6d4c5f8bc1d24d2246783fd
saidevops7722 <saiaws7722@gmail.com>
2023-02-23 11:25:46 +0530

    frontend contents

--- redis.sh
+++ redis.sh
@@ -5,11 +5,11 @@ print_head " installing redis repo files "
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log_file}
 status_check $?
 
-print_heaad " enable 6.2 redis repo file"
+print_head " enable 6.2 redis repo file"
 dnf module enable redis:remi-6.2 -y &>>${log_file}
 status_check $?
 
-print_heaad " installing redis "
+print_head " installing redis "
 yum install redis -y &>>${log_file}
 status_check $?
 
@@ -17,10 +17,10 @@ print_head " update redis listen address "
 sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf /etc/redis/redis.conf &>>${log_file}
 status_check $?
 
-print_heaad " enabling redis service "
+print_head " enabling redis service "
 systemctl enable redis &>>${log_file}
 status_check $?
 
-print_heaad " starting the redis service"
+print_head " starting the redis service"
 systemctl restart redis &>>${log_file}
 status_check $?
