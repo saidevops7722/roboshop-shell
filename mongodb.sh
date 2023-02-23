a03b1d3b8c34f7fc59202fbeee82cf14f927e1d2
saidevops7722 <saiaws7722@gmail.com>
2023-02-23 10:58:34 +0530

    frontend contents

--- mongodb.sh
+++ mongodb.sh
@@ -2,23 +2,23 @@ source common.sh
 
 print_head "copying the mongodb repo files"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
-echo $?
+status_check $?
 
 print_head "installing mongodb"
 yum install mongodb-org -y &>>${log_file}
-echo $?
+status_check $?
 
 print_head "update mongodb listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
-echo $?
+status_check $?
 
 print_head "enabling mongodb"
 systemctl enable mongod &>>${log_file}
-echo $?
+status_check $?
 
 print_head "starting mongodb service"
 systemctl restart mongod &>>${log_file}
-echo $?
+status_check $?
 
 
 
